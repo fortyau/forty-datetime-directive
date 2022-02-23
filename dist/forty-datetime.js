756ac1168a17a9965e69ee5decdb7d5781cd0477
jason summers <jason.summers@fortyau.com>
2022-02-23 15:36:24 -0600

    complyos wrong order

--- dist/forty-datetime.js
+++ dist/forty-datetime.js
@@ -1,5 +1,5 @@
 (function() {
-  angular.module('fortyDate', []).directive(['$filter', '$window', "fortyDatePicker", function($filter, $window) {
+  angular.module('fortyDate', []).directive("fortyDatePicker",['$filter', '$window',  function($filter, $window) {
     return {
       require: "ngModel",
       restrict: "E",
